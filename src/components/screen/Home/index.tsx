fa9b4113a1f5b088e0cda780af3a38f2a6e3bdd7
Jaewoo Park <jaewoopark@Mac-mini.local>
2021-10-10 15:46:26 +0900

    feat : 퀴즈앱 메인 화면 디자인 추가

--- src/components/screen/Home/index.tsx
+++ src/components/screen/Home/index.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import {
 	View,
 	StyleSheet,
+	SafeAreaView,
+	Text,
+	TouchableOpacity,
 } from 'react-native';
 
 
@@ -12,14 +15,43 @@ interface HomeProps {
 const Home = ({ navigation }: HomeProps) => {
 
 	return (
-		<View>
-
-        </View>
+		<View style={styles.container}>
+			<Text style={[styles.Title,{marginTop:120}]}>클래스팅</Text>
+			<Text style={styles.Title}>Quiz</Text>
+			<Text style={{color : "#00008B",marginVertical:20}}>made by jwoo9928</Text>
+			<TouchableOpacity style={styles.play}>
+				<Text style={styles.playtext}>play</Text>
+			</TouchableOpacity>
+		</View>
 	);
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
+	container : {
+		flex:1,
+		backgroundColor:"#1E90FF",
+		alignItems:'center'
+	},
+	Title : {
+		color : "#FFFFFF",
+		fontSize: 50,
+	},
+	play : {
+		borderColor: "#FFFFFF",
+		borderWidth:2,
+		height:120,
+		width:120,
+		borderRadius:60,
+		alignContent:"center",
+		alignItems:"center",
+		marginVertical:100,
+		justifyContent:"center"
+	},
+	playtext : {
+		fontSize:30,
+		color:"#FFFFFF"
+	}
 
 });
